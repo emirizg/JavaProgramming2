802a4d8e65399dfc90b41a6311eba6e39cf31f71
Emir <emirizg77@gmail.com>
2022-09-16 10:23:12 +0200

    day51: Map

--- src/day51_Map/MapPractice2.java
+++ src/day51_Map/MapPractice2.java
@@ -16,26 +16,72 @@ public class MapPractice2 {
         map.put("Conor", 85000);
         map.put("Josh", 117000);
         map.put("Cory", 118000);
-        map.put("Anderson",125000);
+        map.put("Anderson", 125000);
         map.put("Steven", 135000);
 
         //1.1 who has the maximum salary?
+        //1.2 who has the minimum salary?
         String name1 = "";
         int maxSalary = Integer.MIN_VALUE;
+
+        String name2 = "";
+        int minSalary = Integer.MAX_VALUE;
 
         for (Map.Entry<String, Integer> pair : map.entrySet()) {
 
             String eachKey = pair.getKey();
             int eachValue = pair.getValue();
 
-            if (eachValue > maxSalary){
+            if (eachValue > maxSalary) {
                 maxSalary = eachValue;
-                name1=eachKey;
+                name1 = eachKey;
+            }
+
+            if (eachValue < minSalary) {
+                minSalary = eachValue;
+                name2 = eachKey;
             }
 
         }
 
         System.out.println(name1);
+        System.out.println(name2);
+
+        System.out.println("----------------------------------------------------------------");
+
+        //1.3 how many employees has the salary between 120k ~ 150K?
+
+        int count = 0;
+
+        for (Integer eachValue : map.values()) {
+
+            if (eachValue >= 120000 && eachValue <= 150000){
+                count++;
+            }
+
+        }
+
+        System.out.println(count);
+
+        System.out.println("----------------------------------------------------------------");
+
+        //    1.4 display the names of the employees who are making less than 118k?
+        for (Map.Entry<String, Integer> pairs : map.entrySet()) {
+            if ( pairs.getValue() < 118000 ){
+                System.out.println(pairs.getKey());
+            }
+        }
+
+        //1.5 increase the salary of each employee by 10K if the current salary of employee is less than 120k
+
+        System.out.println(map);
+
+        for (Map.Entry<String, Integer> entry : map.entrySet()) {
+            if (entry.getValue() < 120000){
+                entry.setValue(entry.getValue()+10000);
+            }
+        }
+        System.out.println(map);
 
 
     }
